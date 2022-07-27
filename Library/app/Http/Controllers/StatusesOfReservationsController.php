fa91fc6601cae350f902648a2bc81d1b9eadfb28
Pavle <uvrg127@gmail.com>
2022-07-28 01:18:39 +0200

    Transfering to English

---
+++ Library/app/Http/Controllers/StatusesOfReservationsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StatusesOfReservations;
+use App\Http\Requests\StoreStatusesOfReservationsRequest;
+use App\Http\Requests\UpdateStatusesOfReservationsRequest;
+
+class StatusesOfReservationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStatusesOfReservationsRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreStatusesOfReservationsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StatusesOfReservations  $statusesOfReservations
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StatusesOfReservations $statusesOfReservations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StatusesOfReservations  $statusesOfReservations
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StatusesOfReservations $statusesOfReservations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStatusesOfReservationsRequest  $request
+     * @param  \App\Models\StatusesOfReservations  $statusesOfReservations
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStatusesOfReservationsRequest $request, StatusesOfReservations $statusesOfReservations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StatusesOfReservations  $statusesOfReservations
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StatusesOfReservations $statusesOfReservations)
+    {
+        //
+    }
+}
--- Library/database/migrations/New migrations/2022_07_27_231126_create_statuses_of_reservations_table.php
+++ Library/database/migrations/New migrations/2022_07_27_231126_create_statuses_of_reservations_table.php
@@ -15,19 +15,6 @@ return new class extends Migration
     {
         Schema::create('statuses_of_reservations', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger("reservation_id");
-            $table->foreign("reservation_id")
-            ->references("id")
-            ->on("reservations")
-            ->onUpdate("Cascade")
-            ->onDelete("Cascade");
-
-            $table->unsignedBigInteger("reservation_status_id");
-            $table->foreign("reservation_status_id")
-            ->references("id")
-            ->on("reservation_statuses")
-            ->onUpdate("Cascade")
-            ->onDelete("Cascade");
             $table->timestamps();
         });
     }
